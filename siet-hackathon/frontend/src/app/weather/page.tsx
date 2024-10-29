85bce5cbd0f27b38db1d2cd4e83a91bc4aa7081c
Matha Charan <mathacharan3004@gmail.com>
2024-10-30 04:34:25 +0530

    added weather features

--- siet-hackathon/frontend/src/app/weather/page.tsx
+++ siet-hackathon/frontend/src/app/weather/page.tsx
@@ -1,9 +1,15 @@
 import WeatherCard from "@/components/weather/page";
+import IrrigationDashboard from "@/components/irrigation/page";
 
 export default function Weather() {
   return (
-    <div className="grid place-items-center h-[90vh]">
-      <WeatherCard />
+    <div className="flex justify-center items-center h-[90vh]">
+      <div className="w-1/2 p-4"> {/* Left card takes half of the screen */}
+        <WeatherCard />
+      </div>
+      <div className="w-1/2 p-4"> {/* Right card takes half of the screen */}
+        <IrrigationDashboard />
+      </div>
     </div>
   );
 }
